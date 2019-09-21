34db2eec301e1d852583de5482a6c9b7421cb6f2
Calvin Nguyen <cuongvqnguyen@gmail.com>
2019-09-21 15:41:15 -0700

    edit homepage
    adding port
    
    edit app.js
    
    update homepage

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ import express from 'express';
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.listen(8080, () => {
+app.get('/projects', (req, res) => {
+  res.send('Stay tune for upcoming update');
+});
+
+app.listen(process.env.PORT || 8080, () => {
   console.log('Example app listening on port 8080!');
 });
