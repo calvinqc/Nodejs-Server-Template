2c22e83e35bd9dc540110fab7f661998aa6e9515
Calvin Nguyen <cuongvqnguyen@gmail.com>
2019-10-19 14:20:44 -0700

    create react

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from "redux";
 
-import rootReducer from './reducers';
+import rootReducer from "./reducers";
 
-import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
 
 const initialState = {};
 
